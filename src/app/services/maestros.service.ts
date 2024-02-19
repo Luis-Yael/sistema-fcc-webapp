74bb32f355fb0e5b0d73ef2ea2eec2a7919804df
Luis Yael Mendez <luisyaelms@gmail.com>
2024-02-19 17:20:30 -0600

    Formularios
    Se agregan el formulario de los alumnos y el maestro.

---
+++ src/app/services/maestros.service.ts
@@ -0,0 +1,110 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { ValidatorService } from './tools/validator.service';
+import { ErrorsService } from './tools/errors.service';
+import { environment } from 'src/environments/environment';
+import { Observable } from 'rxjs';
+
+const httpOptions = {
+  headers: new HttpHeaders({ 'Content-Type': 'application/json' })
+};
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MaestrosService {
+
+  constructor(
+    private http: HttpClient,
+    private validatorService: ValidatorService,
+    private errorService: ErrorsService
+  ) { }
+
+  public esquemaMaestro(){
+    return {
+      'rol':'',
+      'id_trabajador': '',
+      'first_name': '',
+      'last_name': '',
+      'email': '',
+      'password': '',
+      'confirmar_password': '',
+      'fecha_nacimiento': '',
+      'telefono': '',
+      'rfc': '',
+      'cubiculo': '',
+      'area_investigacion': '',
+      'materias_json': []
+    }
+  }
+
+  //Validación para el formulario
+  public validarMaestro(data: any, editar: boolean){
+    console.log("Validando maestro... ", data);
+    let error: any = [];
+
+    if(!this.validatorService.required(data["id_trabajador"])){
+      error["id_trabajador"] = this.errorService.required;
+    }
+
+    if(!this.validatorService.required(data["first_name"])){
+      error["first_name"] = this.errorService.required;
+    }
+
+    if(!this.validatorService.required(data["last_name"])){
+      error["last_name"] = this.errorService.required;
+    }
+
+    if(!this.validatorService.required(data["email"])){
+      error["email"] = this.errorService.required;
+    }else if(!this.validatorService.max(data["email"], 40)){
+      error["email"] = this.errorService.max(40);
+    }else if (!this.validatorService.email(data['email'])) {
+      error['email'] = this.errorService.email;
+    }
+
+    if(!editar){
+      if(!this.validatorService.required(data["password"])){
+        error["password"] = this.errorService.required;
+      }
+
+      if(!this.validatorService.required(data["confirmar_password"])){
+        error["confirmar_password"] = this.errorService.required;
+      }
+    }
+
+    if(!this.validatorService.required(data["fecha_nacimiento"])){
+      error["fecha_nacimiento"] = this.errorService.required;
+    }
+
+    if(!this.validatorService.required(data["rfc"])){
+      error["rfc"] = this.errorService.required;
+    }else if(!this.validatorService.min(data["rfc"], 12)){
+      error["rfc"] = this.errorService.min(12);
+      alert("La longitud de caracteres deL RFC es menor, deben ser 12");
+    }else if(!this.validatorService.max(data["rfc"], 13)){
+      error["rfc"] = this.errorService.max(13);
+      alert("La longitud de caracteres deL RFC es mayor, deben ser 13");
+    }
+
+    if(!this.validatorService.required(data["telefono"])){
+      error["telefono"] = this.errorService.required;
+    }
+
+    if(!this.validatorService.required(data["cubiculo"])){
+      error["cubiculo"] = this.errorService.required;
+    }
+
+    if(!this.validatorService.required(data["area_investigacion"])){
+      error["area_investigacion"] = this.errorService.required;
+    }
+
+    if(data["materias_json"].length == 0){
+      alert("Debes seleccionar materias para poder registrarte.");
+    }
+    //Return arreglo
+    return error;
+  }
+
+
+}
